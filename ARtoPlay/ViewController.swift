6c3b2c21de59ddb2ce85f7187831b770d4185654
wndzlf <wndzlf@gmail.com>
2018-10-19 11:46:58 +0900

    튜토리얼 따라해보기
    1.Gesture사용 (뭔가 잘 안됨)
    2.Multiple Object (잘 안됨)

--- ARtoPlay/ViewController.swift
+++ ARtoPlay/ViewController.swift
@@ -22,33 +22,69 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        arview.delegate = self
+        //arview.delegate = self
         
-        let text = SCNText(string: "조중현입니다.", extrusionDepth: 1)
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.yellow
-        text.materials = [material]
+//        let text = SCNText(string: "==========================================", extrusionDepth: 1)
+//        let material = SCNMaterial()
+//        material.diffuse.contents = UIColor.yellow
+//        text.materials = [material]
+//
+//        let node = SCNNode()
+//        node.position = SCNVector3(0,0.02,-0.1)
+//        node.scale = SCNVector3(0.01,0.01,0.01)
+//        node.geometry = text
+//        arview.scene.rootNode.addChildNode(node)
+//        arview.automaticallyUpdatesLighting = true
         
-        let node = SCNNode()
-        node.position = SCNVector3(0,0.02,-0.1)
-        node.scale = SCNVector3(0.01,0.01,0.01)
-        node.geometry = text
-        arview.scene.rootNode.addChildNode(node)
-        arview.automaticallyUpdatesLighting = true
+        addBox()
+        addTapGestureToArView()
         
         view.addSubview(arview)
-        arview.showsStatistics = true
-        
-        
+        //arview.showsStatistics = true
         arview.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         arview.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         arview.heightAnchor.constraint(equalTo: view.heightAnchor, constant: -20).isActive = true
     }
+    
+    //Removing Object From ARSCNView
+    func addBox(x: Float = 0 , y: Float = 0 , z: Float = -0.2) {
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(x,y,z)
+        
+        let scene = SCNScene()
+        scene.rootNode.addChildNode(boxNode)
+        arview.scene = scene
+    }
+    
+    //Adding Gesture Recognizer to ARSCNView
+    func addTapGestureToArView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap(withGestureRecognizer:)))
+        arview.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: arview)
+        let hitTestResults = arview.hitTest(tapLocation)
+        guard let node = hitTestResults.first?.node else {
+            let hitTestResultsWithFeaturePoints = arview.hitTest(tapLocation, types: .featurePoint)
+            if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+                let translation = hitTestResultWithFeaturePoints.worldTransform.translation
+                addBox(x: translation.x, y: translation.y, z: translation.z)
+            }
+            return
+        }
+        node.removeFromParentNode()
+    }
+
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         let configuration = ARWorldTrackingConfiguration()
-        
         arview.session.run(configuration)
         //addObject()
     }
@@ -68,7 +104,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let yPos = randomPostion(lowerBound: -1.5, upperBound: 1.5)
         
         ship.position = SCNVector3(xPos, yPos, -1)
-        
         arview.scene.rootNode.addChildNode(ship)
     }
     
@@ -94,3 +129,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 }
 
+//transfroms a matrix into float3. it gives us x, y, z
+extension float4x4{
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
